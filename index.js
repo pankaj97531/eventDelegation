1038d3595eb56532952e62c4179fefe83b117434
pankaj97531 <phpdev.pankaj@gmail.com>
2020-01-18 23:47:11 +0530

    eventdelegation

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var dynamicid = 1;
 document.getElementById("addButton").addEventListener('click',function(){
 	var testvalue = document.getElementById("textid").value;
 	var createNode = document.createElement("LI");
 	var testNode = document.createTextNode(testvalue);
 	var buttonElement = document.createElement("BUTTON");
 	var buttonText = document.createTextNode("Delete");
-	
+	createNode.setAttribute('id','my_li_'+dynamicid);
 	buttonElement.setAttribute("style","color:red;font-size:15px;margin-left:15px;");
 	buttonElement.setAttribute('class',"deletebtn");
 	buttonElement.appendChild(buttonText);
@@ -14,21 +15,25 @@ document.getElementById("addButton").addEventListener('click',function(){
 	document.getElementById("parentul").appendChild(createNode);
 	document.getElementById("textid").value="";
 //	console.log(testvalue);
+	dynamicid++;
 });
 
 document.getElementById("parentul").addEventListener("click",function(e){
 		//console.log(e.toElement.parentElement);
 		//var innerHTMLli = e.toElement.parentElement.innerHTML;
 		//var getInnerHtml = stripHtml(innerHTMLli).replace('Delete','');
-		var getIdElement =  e.toElement.parentElement;
-//		console.log(this.childNodes);
-		console.log(getIdElement.getAttribute('id'));
+		var getId =  e.toElement.parentElement.getAttribute('id');
+		
+		
+		
+		console.log(getId);
 		if(e.toElement.className=='deletebtn'){
 			var innerHTMLli = e.toElement.parentElement.innerHTML;
 			//var outerHTMLBtn = e.toElement.outerHTML;
 			e.toElement.remove();
-
-			console.log(e);
+			var getInnerHtml= document.getElementById(getId).innerHTML;
+			console.log(getInnerHtml);
+			//console.log(e);
 			//e.toElement.parentElement.remove();
 		}
 		//if(e.toElement.)
